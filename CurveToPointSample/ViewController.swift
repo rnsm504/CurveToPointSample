7e8141fe78589d133f34ce9c1b834ea52fcc30de
Kuze Masanori <kuze@MacBookPro.local>
2015-07-07 01:34:49 +0900

    Initial Commit

---
+++ CurveToPointSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CurveToPointSample
+//
+//  Created by Kuze Masanori on 2015/07/07.
+//  Copyright (c) 2015年 Kuze Masanori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
